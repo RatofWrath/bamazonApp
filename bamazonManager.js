87ee1e78fc288aabb10608cc6f712885ebb9d820
RatofWrath <T_mullen93@yahoo.com>
2019-04-14 15:04:58 -0500

    Almost finished just need to do more testing

--- bamazonManager.js
+++ bamazonManager.js
@@ -24,12 +24,120 @@ connection.connect(function(err) {
   console.log("Welcome to the Bamazon Manager menu");
   console.log("Please select an option from the menu below.");
 
+  menu();
+});
+
+function viewInventory(){
+    console.log("Here are our products")
+    console.log("");
+    connection.query("SELECT * FROM products", function(err, res){
+      if(err){
+        console.log("An error has occured");
+        throw err;
+      }
+
+      printInventory(res);
+      menu();
+    })
+}
+
+function getLow(){
+    console.log("Here are our products that we are running low on");
+    console.log("");
+
+    connection.query("SELECT * FROM products WHERE stock_quantity <= 5", function(err, res){
+      if(err){
+        console.log("An error has occured");
+        throw err;
+      }
+
+      printInventory(res);
+      menu();
+    })
+}
+
+function updateInventory(){
+    console.log("What product would you like to add more of?");
+    console.log("");
+
+    inquirer
+   .prompt([
+     {
+       name: "product_id",
+       message: "What is the id of the product you want to change the volume of? "
+     },
+
+     {
+       name: "volume",
+       message: "How many copies would you like there to be?"
+     }
+    ])
+    .then(function(answers){
+      
+      var query = ("UPDATE products SET stock_quantity = ? WHERE item_id = ?");
+      connection.query(query, [answers.volume, answers.product_id]);
+
+      connection.query("SELECT * FROM products", function(err, res){
+        if(err){
+          console.log("An error has occured");
+          throw err;
+        }
+  
+      printInventory(res);
+      menu();
+      })
+  })
+}
+
+function addNewProduct(){
+    console.log("Please enter the details of the product you would like to add.");
+
+    inquirer
+      .prompt([
+        {
+          name: "name",
+          message: "What is the new product's name?"
+        },
+
+        {
+          name: "price",
+          message: "What is this product's price?"
+        },
+
+        {
+          name: "stock",
+          message: "How many should we put in stock?"
+        }
+      ])
+      .then(function(answers){
+
+        var product_name = answers.name;
+        var price = answers.price;
+        var stock = answers.stock;
+
+        var query = "INSERT INTO products (product_name,price,stock_quantity) VALUES(?,?,?)"
+        connection.query(query, [product_name, price, stock]);
+
+        connection.query("SELECT * FROM products", function(err, res){
+          if(err){
+            console.log("An error has occured");
+            throw err;
+          }
+
+        printInventory(res);
+        menu();
+      })
+    })
+
+}
+
+function menu(){
   inquirer
     .prompt([
         {
             type: "list",
             name: "option",
-            choices: ["View Products For Sale", "View Low Inventory", "Add to Inventory", "Add New Product", "Exit"]
+            choices: ["View Products For Sale", "View Low Inventory", "Change Inventory Volume", "Add New Product", "Remove Product", "Exit"]
         }
     ])
     .then(function(answers){
@@ -44,13 +152,17 @@ connection.connect(function(err) {
               getLow();
               break;
 
-            case "Add to Inventory":
+            case "Change Inventory Volume":
               updateInventory();
               break;
 
             case "Add New Product":
               addNewProduct();
               break;
+            
+            case "Remove Product":
+               removeProduct();
+               break;
 
             case "Exit":
               leaveMenu();
@@ -61,26 +173,41 @@ connection.connect(function(err) {
               connection.end();
         }
     });
-});
-
-function viewInventory(){
-    console.log("Here are our products")
-    connection.end();
 }
 
-function getLow(){
-    console.log("Here are our products that we are running low on")
-    connection.end();
+function removeProduct(){
+  inquirer
+   .prompt([
+     {
+       name: "product_id",
+       message: "What is the id of the product you want to remove? "
+     }
+    ])
+     .then(function(answers){
+
+       var query = "DELETE FROM products WHERE ?"
+      connection.query(query, {item_id: answers.product_id});
+
+       connection.query("SELECT * FROM products", function(err, res){
+        if(err){
+          console.log("An error has occured");
+          throw err;
+        }
+
+        printInventory(res);
+        menu();
+     })
+    })
 }
 
-function updateInventory(){
-    console.log("What product would you like to add more of?");
-    connection.end();
-}
-
-function addNewProduct(){
-    console.log("Please enter the details of the product you would like to add.");
-    connection.end();
+function printInventory(res){
+  for(i = 0; i < res.length; i++){
+    console.log("Product ID: " + res[i].item_id);
+    console.log("Product Name: " + res[i].product_name);
+    console.log("Price:  $" + res[i].price);
+    console.log("Quantity Left: " + res[i].stock_quantity);
+    console.log("");
+  }
 }
 
 function leaveMenu(){
